30687d488530b22d2ebdacc7debe2edd25ee51a0
jinkyuhan <gkswlsrb95@gmail.com>
2020-01-22 01:41:01 +0900

    fix: remake ppg module to use PAPAGO openAPI

---
+++ README.md
@@ -0,0 +1,3 @@
+This program needs the api-key to run.
+
+If you want to use this, send email to here 'gkswlsrb95@gmail.com'.
--- ppg.py
+++ ppg.py
@@ -1,90 +1,98 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import sys
-import re
-import time
+#!/usr/bin/python3
 
+import os
+import sys
+import json
+import re
+from urllib import request
 DEBUG = False
 
 
-def main(option, txt):
-    p = re.compile('-[kej]')
-
-    # Option exception
-    if len(option) != 2 or not p.match(option):
-        print("""
-        !!![usage] You can select target language.
-            -k: Korean
-            -e: English
-            -j: Japanese
-            
-            You have to use Double quotation mark for source_txt
-        """)
-    elif option[1] == 'k':
-        print(translate('Korean', txt[1:-1]))
-    elif option[1] == 'e':
-        print(translate('English', txt[1:-1]))
-    elif option[1] == 'j':
-        print(translate('Japanese', txt[1:-1]))
-
-
-def translate(target_lang, source_text) -> str:
-
-    selectors = {
-        "DropdownMenu": "#ddTargetLanguage2 > div.dropdown_top___13QlJ > button:nth-child(2)",
-        "DropdownItem_Korean": "#ddTargetLanguage2 > div.dropdown_menu___XsI_h.active___3VPGL > ul > li.select_item___1U0X9.active___3VPGL > a",
-        "DropdownItem_English": "#ddTargetLanguage2 > div.dropdown_menu___XsI_h.active___3VPGL > ul > li:nth-child(2) > a",
-        "DropdownItem_Japanese": "#ddTargetLanguage2 > div.dropdown_menu___XsI_h.active___3VPGL > ul > li:nth-child(3) > a",
-        "SourceText": "#txtSource",
-        "TargetText": "#txtTarget",
-        "TranslateButton": "#btnTranslate"
-    }
-    # chrome setting
-    setting = webdriver.ChromeOptions()
-    setting.add_argument('headless')
-    setting.add_argument('window-size=1920x1080')
-    setting.add_argument('--disable-gpu')
-
-    # open papago page
-    driver = webdriver.Chrome(
-        '/home/jinkyuhan/Desktop/papago-cli/chromedriver.exe', options=setting)
-    try:
-        driver.get('https://papago.naver.com/')
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-        # select target language
-        dropdown_menu = driver.find_element_by_css_selector(selectors["DropdownMenu"])
-        dropdown_menu.click()
-        dropdown_item = driver.find_element_by_css_selector(selectors["DropdownItem_"+target_lang])
-        dropdown_item.click()
-
-        # input text
-        driver.find_element_by_css_selector(selectors["SourceText"]).send_keys(source_text)
-
-        # Submmit
-        translate_button = driver.find_element_by_css_selector(selectors["TranslateButton"])
-        translate_button.click()
-        time.sleep(1)
-        # read result
-        result = driver.find_element_by_css_selector(selectors["TargetText"]).text
-
+def detect_language(text):
+    url = "https://openapi.naver.com/v1/papago/detectLangs"
+    data = 'query="{}"'.format(text)
+    req = request.Request(url)
+    req.add_header("X-Naver-Client-Id", CLIENT_ID)
+    req.add_header("X-Naver-Client-Secret", CLIENT_SECRET)
+    res = request.urlopen(req, data=data.encode("utf-8"))
+    res_code = res.getcode()
+    if(res_code == 200):  # success
+        res_body = res.read()
+        dec_data = res_body.decode('utf-8')
         if DEBUG:
-            driver.get_screenshot_as_file("dbjg.png")
-        
-    finally:
-        driver.quit()
+            print("[Response] detected_lang: ", dec_data)
+        detected_lang = json.loads(dec_data)["langCode"]
+        return detected_lang
+    else:  # fail
+        print("! ERROR CODE: ", res_code)
+        sys.exit()
 
-    return result
+
+def translate(target_lang, source_text):
+    source_lang = detect_language(source_text)
+    if source_lang == target_lang:
+        print("! BAD OPTION: The source_lang and target_lang could not be same")
+        sys.exit()
+    url = "https://openapi.naver.com/v1/papago/n2mt"
+    data = 'source={}&target={}&text="{}"'.format(
+        source_lang, target_lang, source_text)
+    req = request.Request(url)
+    req.add_header("X-Naver-Client-Id", CLIENT_ID)
+    req.add_header("X-Naver-Client-Secret", CLIENT_SECRET)
+    res = request.urlopen(req, data=data.encode("utf-8"))
+    res_code = res.getcode()
+    if(res_code == 200):  # success
+        res_body = res.read()
+        dec_data = res_body.decode('utf-8')
+        if DEBUG:
+            print("[Response] translated_txt:", dec_data)
+        translated_text = json.loads(
+            dec_data)["message"]["result"]["translatedText"]
+        return translated_text
+    else:  # fail
+        print("! ERROR CODE: ", res_code)
+        sys.exit()
+
+
+def get_api_key():
+    with open('./API_key.json') as API_key:
+        key = json.load(API_key)
+    return key
+
+
+def main(option, txt):
+
+    if option[1] == 'k':
+        print(translate('ko', txt))
+    elif option[1] == 'e':
+        print(translate('en', txt))
+    elif option[1] == 'j':
+        print(translate('ja', txt))
 
 
 if __name__ == "__main__":
 
-    # Excution arguments exception
-    if len(sys.argv) != 3:
+    # Bad arguments exception
+    p = re.compile('-[kej]')
+
+    if len(sys.argv) != 3 or len(sys.argv[1]) != 2 or not p.match(sys.argv[1]):
         print("""
-        !!! usage: ppg [target_lang_option] "[source_txt]"
-            
-            You have to use Double quotation mark for source_txt
+        Bad arugments ERROR!
+
+        Usage: ppg [target_lang_option] "[source_txt]"
+         
+        You have to select target language and use double quatoation for blank of source_txt.
+            -k: Korean
+            -e: English
+            -j: Japanese
+        
+        You have to use Double quotation mark for source_txt like this
+            ~/$ ppg -k "hello"
         """)
         sys.exit()
+    
+    key = get_api_key()
+    CLIENT_ID = key['client_id']
+    CLIENT_SECRET = key['client_secret']
+
     main(sys.argv[1], sys.argv[2])
