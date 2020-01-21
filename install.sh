7234572750336945b715f774fc6bb854cc7f1812
jinkyuhan <gkswlsrb95@gmail.com>
2020-01-22 03:09:07 +0900

    fix: change relative path to absolute path

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo cp ./ppg.py /usr/bin/ppg
-sudo chmod +x /usr/bin/ppg
+sudo mkdir /usr/share/ppg
+sudo cp ./API_key.json /usr/share/ppg/API_key.json
+sudo cp ./ppg.py /usr/share/ppg/ppg
+sudo chmod +x /usr/share/ppg/ppg
+sudo ln -s /usr/share/ppg/ppg /usr/bin/ppg
 
 echo "Install Complete"
 echo "Usage : ppg [-e or -k or -j] [source_text]"
