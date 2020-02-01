937dae0ba0177105a5ac0efb2bcc38324233008d
jinkyuhan <gkswlsrb95@gmail.com>
2020-02-01 22:47:07 +0000

    feat: automately download the api-key from googledrive

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "#!/bin/bash" > ./uninstall.sh
 echo "" >> ./uninstall.sh 
 echo "sudo rm -f /usr/bin/ppg" >> ./uninstall.sh
 echo "sudo rm -rf /usr/share/ppg" >> ./uninstall.sh
-echo "echo \"Complete to uninstall papago-cli. You can download it again in https://github.com/jinkyuhan/papago-cli\"" >> ./uninstall.sh
+echo "echo \"Complete to uninstall papago-cli clearly. You can download it again in https://github.com/jinkyuhan/papago-cli\"" >> ./uninstall.sh
 
 chmod +x ./uninstall.sh
 
@@ -15,7 +15,7 @@ chmod +x ./uninstall.sh
 # install papago-cli
 if [ ! -d /usr/share/ppg ]; then
 	sudo mkdir /usr/share/ppg
-	sudo cp ./API_key.json /usr/share/ppg/API_key.json
+	sudo wget -O /usr/share/ppg/papagoapikey.json https://drive.google.com/uc?id=1W9bdvRnwvKV6podahUMgxNsUgr9m4fAf
 	sudo cp ./ppg.py /usr/share/ppg/ppg
 	sudo chmod +x /usr/share/ppg/ppg
 	sudo ln -s /usr/share/ppg/ppg /usr/bin/ppg
